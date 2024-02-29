4c896eda5bbe7a5c7193413da65886b414ba5027
SajidAhmed0 <mjsahmed205@gmail.com>
2024-03-01 01:44:13 +0530

    get employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { DatePipe, CurrencyPipe } from '@angular/common';
 
 
 @Component({
@@ -15,18 +22,64 @@ import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
     MatToolbarModule, 
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
+    DatePipe,
+    CurrencyPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'crud-app';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id', 
+    'firstName', 
+    'lastName', 
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package',
+    'action'
+  ];
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private _dialog: MatDialog) {}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _dialog: MatDialog, private _empService: EmployeeService) {}
 
   openAddEditEmpForm(){
     this._dialog.open(EmpAddEditComponent);
   }
 
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+
+  getEmployeeList() {
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+         this.dataSource = new MatTableDataSource(res);
+         this.dataSource.sort = this.sort;
+         this.dataSource.paginator = this.paginator;
+      },
+      error: console.log
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
